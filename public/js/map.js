f80399f823eff14a47699ae82b91ee5017d7deec
AllanNguyen <allandog06@gmail.com>
2024-10-28 18:39:36 -0700

    added a home page that shows current shows BCIT

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,20 @@
 import apiKey from "./GoogleAPI_BBY4.js";
 
-function initMap() {
-    console.log(apiKey);
+window.initMap = function() {
+    const mapOptions = {
+        center: { lat: 49.248, lng: -123.0005},
+        zoom: 15.5, // Zoom level
+    };
+
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
-initMap();
+
+function LoadGoogleMaps() {
+    let script = document.createElement("script");
+    script.setAttribute("src", "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&loading=async&callback=initMap");
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+LoadGoogleMaps();
 
