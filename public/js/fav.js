739158fbd63e95def6d77a0849e40884417da428
John  <jyeh0630@gmail.com>
2024-11-05 17:33:40 -0800

    added fav

--- public/js/fav.js
+++ public/js/fav.js
@@ -1,6 +1,18 @@
 function displayFav() {
     let user = db.collection("User").doc(userUID);
+    let ID = params.searchParams.get("docID");
 
+    db.collection("User").doc(ID).collection("favourites")
+        .get()
+        .then(doc => {
+            thisFav = doc.data();
+            Classroom = doc.data().Classroom;
+            Building = doc.data().Building;
+
+            document.getElementById("Building").innerHTML = Building;
+            document.getElementById("Classroom").innerHTML = Classroom;
+            
+        })
 
 }
 displayFav();
@@ -11,7 +23,7 @@ function addFav() {
 
         // Get the document for the current user.
         db.collection("Users").doc(userID).collection("favourites").dooc().add({
-            Classroom: CRN,
+            Classroom: Classroom,
             userID: userID,
             Building: BuildingName,
             Location: Geopoint,
