73cd15e8511659fa33b037a3788973eb84b1d317
John  <jyeh0630@gmail.com>
2024-11-23 10:58:18 -0800

    add feeback for request

--- public/js/request.js
+++ public/js/request.js
@@ -1,13 +1,18 @@
 async function writeRequest() {
     let user = firebase.auth().currentUser;
 
-    let input1 = document.getElementById("description").value;
-    let input2 = document.getElementById("building").value;
-    db.collection("Requests").add({
-        user: user.displayName,
-        Request: input1,
-        building: input2
-    });
+    if (!confirm('Are you sure?')) {
+        e.preventDefault();
+    } else {
+        let input1 = document.getElementById("description").value;
+        let input2 = document.getElementById("building").value;
+        db.collection("Requests").add({
+            user: user.displayName,
+            Request: input1,
+            building: input2
+        });
+    }
+
 }
 
 function readReview() {
