7e51e6ece5db846a220e7bfd294e7f183d8a73c9
MohamedBCIT <mmohamed32@my.bcit.ca>
2024-12-03 01:35:54 -0800

    added a front end contact info page and edited the request pages colours.

--- main.js
+++ main.js
@@ -48,6 +48,10 @@ app.get("/home/thanks", function (req, res) {
     let doc = fs.readFileSync("./app/html/thanks.html", "utf-8");
     res.send(doc);
 })
+app.get("/home/contact", function (req, res) {
+    let doc = fs.readFileSync("./app/html/contact.html", "utf-8");
+    res.send(doc);
+})
 let port = 8000;
 app.listen(port, function () {
     console.log("Listening on port " + port);
